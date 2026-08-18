a7c4f1c4fabdd4e2117fef7f45183bbde442f72d
iwariku <02rikuto18@gmail.com>
2026-08-18 10:59:24 +0900

    タスクを更新できるように

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   post "tasks", to: "tasks#create", as: :tasks
   get "tasks", to: "tasks#index"
   get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
   root "home#index"
 end
