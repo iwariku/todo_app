ac6cc153ccf8d45128f0cd4383b7f316e46a8e20
iwariku <02rikuto18@gmail.com>
2026-08-18 09:17:10 +0900

    タスク詳細を表示できるように

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get "tasks/new", to: "tasks#new", as: :new_task
   post "tasks", to: "tasks#create", as: :tasks
   get "tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: :task
   root "home#index"
 end
